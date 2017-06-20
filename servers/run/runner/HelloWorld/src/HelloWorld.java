b1fa941e29c2cb9a58340988fff459b9a28a4fa8
souparno majumder <souparno.majumder@gmail.com>
2017-06-20 16:03:44 +0530

    Hello world program added in runner

--- servers/run/runner/HelloWorld/src/HelloWorld.java
+++ servers/run/runner/HelloWorld/src/HelloWorld.java
@@ -1,19 +1,11 @@
 import java.io.*;
 
 public class HelloWorld
-{
-	/*public String helloworld(String a)
-	{
-		String b="Hello World!";
-		return b;
-	}*/	
+{	
 
 
 	public static void main() throws java.io.IOException{
-	  //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	  //String a= br.readLine();
-	  //HelloWorld obj = new HelloWorld();
-	  //String b = obj.helloworld(a);
- 	  System.out.println("Hello World!");
+	//---------------------------------------------------------
+	System.out.println("Hello World!");
 	}
 }
--- servers/run/runner/HelloWorld/test/HelloWorldTest.java
+++ servers/run/runner/HelloWorld/test/HelloWorldTest.java
@@ -11,35 +11,34 @@ import java.lang.reflect.*;
 public class HelloWorldTest extends Base
  {
 
-   /* @Test 
-    public void t1() {
-      description = "1.lets print 'Hello World' using 'helloworld'";
-    
-      String a = "Hello World";
-      String b ="";	
-      HelloWorld obj = new HelloWorld();
-      try{
-	Method m=getMethod(obj,"helloworld");
-		if(m!=null)
-		{
-                        b = m.invoke(obj,a).toString();
-			assertEquals("Hello World!",b);		
-		}
-	}
-	catch(Exception e){}
-    }*/
    @Test
    public void t1() throws java.io.IOException{
-         description = "call the main fucntion";
+         description = "Initialize a main function using throws IOException ,Example-public static void main() throws java.io.IOException{program}";
 	HelloWorld obj = new HelloWorld();
-	//ByteArrayInputStream in = new ByteArrayInputStream("Hello World".getBytes());
-	//System.setIn(in);
 	try {
 		Method m=getMethod(obj,"main");
-		if(m!=null)
+	        assertEquals(m.getName(),"main");
+     
+
+    		}
+        
+	catch(Exception e){}
+    
+        }
+
+   
+ 
+
+    @Test
+   public void t2() throws java.io.IOException{
+         description = "Within the code part of main write the code line to print 'Hello World'";
+	HelloWorld obj = new HelloWorld();
+	try {
+		Method m=getMethod(obj,"main");
+		if(m!= null)
 		{
-		   m.invoke(obj);
-		   assertEquals("Hello World!\n", outContent.toString());
+		m.invoke(obj);
+	        assertEquals("Hello World!\n", outContent.toString());
      
 
     		}
@@ -49,6 +48,7 @@ public class HelloWorldTest extends Base
         }
 
    }
+
 
     
 
