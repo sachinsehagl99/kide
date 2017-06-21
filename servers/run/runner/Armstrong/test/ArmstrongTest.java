ec9787f87c54143eabbf4ccba5bfe17c52fe38b6
Saikat Mondal <saikatmondal15@gmail.com>
2017-06-21 19:32:30 +0530

    Descriptions of Armstrong and prime modified

--- servers/run/runner/Armstrong/test/ArmstrongTest.java
+++ servers/run/runner/Armstrong/test/ArmstrongTest.java
@@ -11,7 +11,7 @@ public class ArmstrongTest extends Base
  {
      @Test
    public void t1() throws java.io.IOException{
-        description = "Initialize The main function 'public static void main(String args[])throws IOException{ code }' to write your program within the braces";
+        description = "1.Initialize The main function\nEx- 'public static void main()throws IOException{}'";
 	Armstrong obj = new Armstrong();
 	try {
 		Method m=getMethod(obj,"main");
@@ -24,7 +24,7 @@ public class ArmstrongTest extends Base
  }
    @Test
    public void t2() throws java.io.IOException{
-        description = "Within the braces of main write the code to input a number using the tag 'Input The number' in a integer type varriable";
+        description = "2.Write a print Statement and print 'Input the number'\n3.Write the input statement and Input a integer value.";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("371".getBytes());
 	System.setIn(in);
@@ -43,7 +43,7 @@ public class ArmstrongTest extends Base
     	        }
 	@Test
 	public void t3() throws java.io.IOException{
-        description = "Within the braces of main write a code to copy the inputed value into another integer type varriable and then print it using System.out.println";
+        description = "4.Copy the inputed value into another integer type varriable and then print it using print statement";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("371".getBytes());
 	System.setIn(in);
@@ -64,7 +64,7 @@ public class ArmstrongTest extends Base
 
 	@Test
 	public void t4() throws java.io.IOException{
-        description = "Now use a do while loop to extract the digits of the number and print the digits at every step";
+        description = "5.Now use a do-while loop to extract the digits of the number and print the digits at every step";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("371".getBytes());
 	System.setIn(in);
@@ -84,7 +84,7 @@ public class ArmstrongTest extends Base
         }
 	@Test
 	public void t5() throws java.io.IOException{
-        description = "Within the do while loop find the sum of the cube of the digits and print at every step";
+        description = "6.Next within the do-while loop find the sum of the cube of the digits and print it at every step";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("371".getBytes());
 	System.setIn(in);
@@ -105,7 +105,7 @@ public class ArmstrongTest extends Base
 
 	@Test
 	public void t6() throws java.io.IOException{
-        description = "After Ending the while loop check the summation of cube of digits with the duplicate of inputed number using a if else statement to check if it is armstrong or not";
+        description = "7.After Ending the while loop, check the resultant value with the duplicate of inputed number using a if statement to check if it is an armstrong number or not";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("371".getBytes());
 	System.setIn(in);
@@ -126,7 +126,7 @@ public class ArmstrongTest extends Base
 
 	@Test
 	public void t7() throws java.io.IOException{
-        description = "After Ending the while loop check the summation of cube of digits with the duplicate of inputed number using a if else statement to check if it is armstrong or not";
+        description = "8.After Ending the if statement,use else statement to print 'The Number is not an Armstrong Number'";
 	Armstrong obj = new Armstrong();
 	ByteArrayInputStream in = new ByteArrayInputStream("121".getBytes());
 	System.setIn(in);
@@ -140,9 +140,21 @@ public class ArmstrongTest extends Base
      		
     		}
         }
-	catch(Exception e){ System.err.println(e);
+	catch(Exception e){ System.err.println(e);}
 }
-    	    
-        }
+    	@Test
+       	public void t8() {
+	    description="You have succefully Compeleted the program!!!!";
+	    
+	    
+	    try
+	    {
+		    assertEquals(0,0);
+	    }
+	    catch(Exception e){
+	    }
+	
+	}	
+        
 
  } 
