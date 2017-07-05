10dba07a89445ec26581790cb808d50b328157e2
souparno majumder <souparno.majumder@gmail.com>
2017-07-06 03:20:20 +0530

    class path added while building the srcTemkplate file

--- servers/run/runner/index.js
+++ servers/run/runner/index.js
@@ -5,10 +5,11 @@ module.exports = function(name, testMethod, build_dir, src_file, test_build_dir,
   var hamcrest_path =  __dirname + '/lib/hamcrest-core-1.3.jar';
   var junit_json_runner =  __dirname + '/lib/junit-json-runner.jar';
   var json_simple = __dirname + '/lib/json-simple-1.1.1.jar';
-  shelljs.exec('javac -d ' + build_dir + ' ' + src_file, function(code, output) {
+  var class_path = junit_json_runner;
 
+  shelljs.exec('javac -d ' + build_dir + ' -cp ' + class_path + ' ' + src_file, function(code, output) {
       if (!code) {
-         var class_path  =  junit_path +':' + build_dir +':' + junit_json_runner;
+         class_path  =  junit_path +':' + build_dir +':' + junit_json_runner;
          shelljs.exec('javac -d ' + test_build_dir + ' -cp ' + class_path + ' ' + test_file, function(code, output) {
            if (!code) {
 		class_path = hamcrest_path + ':' + junit_path + ':' + test_build_dir +':' + junit_json_runner +':' + json_simple  + ":" + build_dir;
